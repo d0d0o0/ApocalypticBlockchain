cfce920133b6106c00199728c2dcd7f72f42b270
e80080u <e80080u>
2025-12-05 11:50:59 +0100

    Block class added

---
+++ Blockchain.py
@@ -0,0 +1,15 @@
+class Blockchain:
+    chain = list()
+    mempool = list()
+
+    def __init__(self):
+        pass
+
+    def add_transaction(self, transaction):
+        self.mempool.append(transaction)
+
+    def create_block():
+        pass
+
+    def validate_block():
+        pass
