365f8297d59e98b1f20939db0a8d7a43b1c7c704
roromario <romain.bezai@gmail.com>
2025-12-05 11:15:37 +0100

    envoie fichier des biblio a download et classe user

---
+++ User.py
@@ -0,0 +1,13 @@
+from Crypto.PublicKey import RSA
+from hashlib import hash
+
+class User :
+    def __init__(self, nom):
+        key = RSA.generate(2048)
+        self.secretKey = key.export_key()
+        self.publicKey = key.public_key().export_key
+        self.addressID = hash.sha256(self.publicKey)
+        self.pseudo = nom
+
+    def getPseudo(self):
+        return hash.sha256(self.publicKey)
